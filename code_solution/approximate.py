1ca17d7fc188047045fb3ac648a8d2e8f7cdb249
NickBoychenko <boychena@dukes.jmu.edu>
2024-12-09 21:32:04 -0500

    comment runtime of approx

--- code_solution/approximate.py
+++ code_solution/approximate.py
@@ -1,17 +1,22 @@
 import sys
 
 def solve_vertex_cover():
+    # O(m) --
     lines = sys.stdin.read().strip().split('\n')
     m = int(lines[0])
+    # --
     
-    # Parse edges
+
+    # Parse edges O(m) --
     edges = []
     for i in range(1, m+1):
         u, v = lines[i].split()
         edges.append((u, v))
+    # --
 
     vertex_cover = set()
     
+    # O(m) --
     for (u, v) in edges:
         # If either vertex is in the cover, skip
         if u in vertex_cover or v in vertex_cover:
@@ -20,8 +25,11 @@ def solve_vertex_cover():
         # Add both edges
         vertex_cover.add(u)
         vertex_cover.add(v)
+    # --
 
+    # O(n) --
     print(" ".join(vertex_cover))
+    # --
 
 
 if __name__ == "__main__":
