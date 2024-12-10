3d68e71e28ff9f0c4bac5803f31bf1ec7b459a19
NickBoychenko <boychena@dukes.jmu.edu>
2024-12-09 21:18:42 -0500

    run tests on both approx and exact

--- code_solution/run_test_cases.sh
+++ code_solution/run_test_cases.sh
@@ -1,7 +1,14 @@
-for test_file in test_cases/*; do
-    if [ -f "$test_file" ]; then
-        echo "Running test on $test_file"
-        python approximate.py < "$test_file"
-        echo
-    fi
+#!/bin/bash
+
+solutions=("approximate.py" "exact.py")
+
+for i in "${solutions[@]}"; do
+    echo "Testing on $i"
+    for test_file in test_cases/*; do
+        if [ -f "$test_file" ]; then
+            echo "Running test on $test_file"
+            python "$i" < "$test_file"
+            echo
+        fi
+    done
 done
